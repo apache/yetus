358b0eab7ff2eca2583c8ecef871680e9044fb4d
Kengo Seki <sekikn@gmail.com>
2015-10-31 00:20:42 +0900

    YETUS-124. add exemplar drill personality
    Signed-off-by: Allen Wittenauer <aw@apache.org>

---
+++ precommit/personality/drill.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+personality_plugins "all"
+
+function personality_globals
+{
+  #shellcheck disable=SC2034
+  GITHUB_REPO="apache/drill"
+  #shellcheck disable=SC2034
+  HOW_TO_CONTRIBUTE="https://drill.apache.org/docs/apache-drill-contribution-guidelines/"
+  #shellcheck disable=SC2034
+  JIRA_ISSUE_RE='^DRILL-[0-9]+$'
+  #shellcheck disable=SC2034
+  JIRA_STATUS_RE='Reviewable'
+}
