4f6a369a4108e40aad8981dd2cc41ad0ca7ae0bd
Allen Wittenauer <aw@apache.org>
2015-11-17 14:57:47 -0800

    Revert "YETUS-176. hadoop: mvn site tests aren't getting run (trial)"
    This reverts commit fa123289709bed6891f9ea4aafba6b55ae884e90.

--- precommit/personality/hadoop.sh
+++ precommit/personality/hadoop.sh
@@ -327,14 +327,13 @@ function personality_file_tests
        || ${filename} =~ src/main/conf
        ]]; then
     yetus_debug "tests/shell: ${filename}"
-    add_test mvnsite
     add_test unit
   elif [[ ${filename} =~ \.md$
        || ${filename} =~ \.md\.vm$
        || ${filename} =~ src/site
        ]]; then
     yetus_debug "tests/site: ${filename}"
-    add_test mvnsite
+    add_test site
   elif [[ ${filename} =~ \.c$
        || ${filename} =~ \.cc$
        || ${filename} =~ \.h$
@@ -344,22 +343,18 @@ function personality_file_tests
        || ${filename} =~ CMakeLists.txt
        ]]; then
     yetus_debug "tests/units: ${filename}"
-    add_test compile
     add_test cc
-    add_test mvnsite
-    add_test javac
     add_test unit
+    add_test javac
   elif [[ ${filename} =~ build.xml$
        || ${filename} =~ pom.xml$
        || ${filename} =~ \.java$
        || ${filename} =~ src/main
        ]]; then
       yetus_debug "tests/javadoc+units: ${filename}"
-      add_test compile
       add_test javac
       add_test javadoc
       add_test mvninstall
-      add_test mvnsite
       add_test unit
   fi
 
