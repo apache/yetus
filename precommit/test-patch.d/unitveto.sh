6566f91dbde6b4fa2c052289a8c122eb0526350a
Allen Wittenauer <aw@apache.org>
2016-01-22 13:17:15 -0800

    YETUS-272. add a way to flag/veto patches to code which jenkins doesn't test
    Signed-off-by: Allen Wittenauer <aw@apache.org>

---
+++ precommit/test-patch.d/unitveto.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+add_test_type unitveto
+
+UNITVETO_RE=${UNITVETO_RE:-}
+
+function unitveto_filefilter
+{
+  declare filename=$1
+
+  if [[ -n "${UNITVETO_RE}"
+     && ${filename} =~ ${UNITVETO_RE} ]]; then
+    yetus_debug "unitveto: ${filename} matched"
+    add_test unitveto
+  fi
+}
+
+function unitveto_usage
+{
+  yetus_add_option "--unitveto-re=<regex>" "Regex to automatically -1 due to manual test requirements"
+}
+
+function unitveto_parse_args
+{
+  declare i
+
+  for i in "$@"; do
+    case ${i} in
+      --unitveto-re=*)
+        UNITVETO_RE=${i#*=}
+      ;;
+    esac
+  done
+}
+
+function unitveto_patchfile
+{
+  verify_needed_test unit
+
+  if [[ $? == 0 ]]; then
+    return 0
+  fi
+
+  verify_needed_test unitveto
+
+  if [[ $? == 0 ]]; then
+    return 0
+  fi
+
+  add_vote_table -1 unitveto "Patch requires manual testing."
+  return 1
+}
