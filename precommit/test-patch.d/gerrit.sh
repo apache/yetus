40a7af3377d8087779bf8ad66397947b7270737a
Suraj Acharya <suraj.spa@gmail.com>
2017-06-18 10:31:32 -0500

    Add support to  write comments to gerrit from yetus
    Signed-off-by: Suraj Acharya <suraj.spa@gmail.com>

--- precommit/test-patch.d/gerrit.sh
+++ precommit/test-patch.d/gerrit.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# this bug system handles GERRIT.  Personalities
-# can override the following variables:
+# this bug system handles GERRIT.
+# Personalities can override the following variables:
 
 add_bugsystem gerrit
 
@@ -25,7 +25,10 @@ function gerrit_usage
 {
   yetus_add_option "--gerrit-password=<pw>" "The password for the 'jira' command"
   yetus_add_option "--gerrit-user=<user>" "The user for the 'jira' command"
-  yetus_add_option "--gerrit-location=<hostname:port>" "The URL of gerrit"
+  yetus_add_option "--gerrit-location=<hostname>" "The URL of gerrit"
+  yetus_add_option "--gerrit-port=<port>" "The port of gerrit"
+  yetus_add_option "--gerrit-patchset=<patchset>" "The patchset being tested"
+  yetus_add_option "--gerrit-changenumber=<changenumber>" "The change Number of the change"
 }
 
 function gerrit_parse_args
@@ -48,6 +51,18 @@ function gerrit_parse_args
         GERRIT_ADDRESS=${i#*=}
         yetus_debug "Gerrit Address is ${GERRIT_ADDRESS}"
       ;;
+      --gerrit-port=*)
+        GERRIT_PORT=${i#*=}
+        yetus_debug "Gerrit Port is ${GERRIT_PORT}"
+      ;;
+      --gerrit-patchset=*)
+        GERRIT_PATCHSET_NUMBER=${i#*=}
+        yetus_debug "Gerrit Patchset is ${GERRIT_PATCHSET_NUMBER}"
+      ;;
+      --gerrit-changenumber=*)
+        GERRIT_CHANGENUMBER=${i#*=}
+        yetus_debug "Gerrit Change Number is ${GERRIT_CHANGENUMBER}"
+      ;;
     esac
   done
 }
@@ -69,7 +84,8 @@ function gerrit_locate_patch
   yetus_debug "Storing the patch at : ${output}"
 
   #create URL to download from. Always use the current revision. Gets the latest
-  URL="https://${GERRIT_ADDRESS}/changes/${input}/revisions/current/patch?zip"
+  GERRIT_REVISION=${GERRIT_PATCHSET_NUMBER:-current}
+  URL="https://${GERRIT_ADDRESS}/changes/${input}/revisions/${GERRIT_REVISION}/patch?zip"
   yetus_debug "URL to download from ${URL}"
 
   # the actual patch file
@@ -84,8 +100,107 @@ function gerrit_locate_patch
   unzip ${output}.zip -d ${output}-zip
   mv -v ${output}-zip/*.diff ${output}
 
-  add_footer_table "Gerrit ChangeID : ${input} Gerrit Host : ${GERRIT_ADDRESS}"
-
   return 0
 }
 
+function gerrit_finalreport
+{
+  declare result=$1
+  declare i
+  declare commentfile=${PATCH_DIR}/gerritcommentfile
+  declare comment
+  declare vote
+  declare ourstring
+  declare ela
+  declare subs
+  declare comment
+  declare calctime
+
+  big_console_header "Adding comment to Gerrit"
+
+  if [[ "${OFFLINE}" == true ]]; then
+      yetus_debug "gerrit_update_review: offline, skipping"
+      return 1
+  fi
+
+  rm "${commentfile}" 2>/dev/null
+
+  echo "'" >>  "${commentfile}"
+
+  if [[ ${result} == 0 ]]; then
+    echo "| (/) *+1 overall* |" >> "${commentfile}"
+    overall=+1
+  else
+    echo "| (x) *-1 overall* |" >> "${commentfile}"
+    overall=-1
+  fi
+
+  printf "\n\n" >>  "${commentfile}"
+
+  i=0
+  until [[ $i -eq ${#TP_HEADER[@]} ]]; do
+    printf "%s\n" "${TP_HEADER[${i}]}" >> "${commentfile}"
+    ((i=i+1))
+  done
+
+  echo "|| Vote || Subsystem || Runtime || Comment ||" >> "${commentfile}"
+
+  i=0
+  until [[ $i -eq ${#TP_VOTE_TABLE[@]} ]]; do
+    ourstring=$(echo "${TP_VOTE_TABLE[${i}]}" | tr -s ' ')
+    vote=$(echo "${ourstring}" | cut -f2 -d\| | tr -d ' ')
+    subs=$(echo "${ourstring}"  | cut -f3 -d\|)
+    ela=$(echo "${ourstring}" | cut -f4 -d\|)
+    calctime=$(clock_display "${ela}")
+    comment=$(echo "${ourstring}"  | cut -f5 -d\|)
+
+    printf "| %s | %s | %s | %s |\n" \
+      "${vote}"  "${subs}" "${calctime}" "${comment}" \
+      >> "${commentfile}"
+    ((i=i+1))
+  done
+
+  if [[ ${#TP_TEST_TABLE[@]} -gt 0 ]]; then
+    printf "\n\n" >>  "${commentfile}"
+
+    echo "|| Reason || Tests ||" >>  "${commentfile}"
+    i=0
+    until [[ $i -eq ${#TP_TEST_TABLE[@]} ]]; do
+      printf "%s\n" "${TP_TEST_TABLE[${i}]}" >> "${commentfile}"
+      ((i=i+1))
+    done
+  fi
+
+  { echo "\\\\" ; echo "\\\\"; } >>  "${commentfile}"
+
+  echo "|| Subsystem || Report/Notes ||" >> "${commentfile}"
+  i=0
+  until [[ $i -eq ${#TP_FOOTER_TABLE[@]} ]]; do
+    comment=$(echo "${TP_FOOTER_TABLE[${i}]}" |
+              ${SED} -e "s,@@BASE@@,${BUILD_URL}${BUILD_URL_ARTIFACTS},g")
+    printf "%s\n" "${comment}" >> "${commentfile}"
+    ((i=i+1))
+  done
+
+  printf "\n\nThis message was automatically generated.\n\n" >> "${commentfile}"
+
+  echo  "'" >> "${commentfile}"
+
+  yetus_debug "Gerrit comment placed at : ${commentfile}"
+  yetus_debug "Hostname : ${GERRIT_ADDRESS} and Port Number : ${GERRIT_PORT}"
+
+  if [[ -z ${GERRIT_CHANGENUMBER} ]]; then
+    yetus_debug "Gerrit change number is needed for posting comment."
+    return 0
+  fi
+  if [[ -z ${GERRIT_PATCHSET_NUMBER} ]]; then
+    yetus_debug "Gerrit patchset number is needed for posting comment."
+    return 0
+  fi
+
+ GERRIT_USER=${GERRIT_USER:-jenkins}
+  ssh -p ${GERRIT_PORT} ${GERRIT_USER}@${GERRIT_ADDRESS}  gerrit review \
+  ${GERRIT_CHANGENUMBER},${GERRIT_PATCHSET_NUMBER} \
+   --verified ${overall} -m "$(cat ${commentfile})"
+
+}
