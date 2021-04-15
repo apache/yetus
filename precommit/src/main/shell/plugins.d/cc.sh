18379710edf06c06519cd3bcc17af370beed65e4
Masatake Iwasaki <iwasakims@apache.org>
2021-04-15 07:51:10 +0000

    YETUS-1107. calcdiffs of cc raise false positive warnings.

--- precommit/src/main/shell/plugins.d/cc.sh
+++ precommit/src/main/shell/plugins.d/cc.sh
@@ -68,5 +68,5 @@ function cc_logfilter
   declare output=$2
 
   #shellcheck disable=SC1117
-  "${GREP}" -i -E "^.*\.${CC_EXT_RE}\:[[:digit:]]*\:" "${input}" > "${output}"
+  "${GREP}" -i -E "^.*\.${CC_EXT_RE}\:[[:digit:]]*\:" "${input}" | sort > "${output}"
 }
